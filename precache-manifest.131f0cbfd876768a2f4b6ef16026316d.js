5a6e04f06ca136ba5ee2e07b20a54327ad86611a
aktarulrahul <rahul.aktarul@gmail.com>
2020-10-22 06:31:55 +0600

    Updates

---
+++ precache-manifest.131f0cbfd876768a2f4b6ef16026316d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b754b644983bc8d731a790f23d13d93",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3c3ddea7cf60c2ea38f7",
+    "url": "/monsters-rolodex/static/css/main.35d927dd.chunk.css"
+  },
+  {
+    "revision": "0207cdc40cb785af9389",
+    "url": "/monsters-rolodex/static/js/2.1b1c837d.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/monsters-rolodex/static/js/2.1b1c837d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c3ddea7cf60c2ea38f7",
+    "url": "/monsters-rolodex/static/js/main.d2555a2f.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
